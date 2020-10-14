0ae9aecffad51574021b74b1f863495a5b264535
Shelby <71155281+SWHurdyGurdyMan@users.noreply.github.com>
2020-10-14 13:18:25 -0500

    Add files via upload
    final commit

--- UniqueNames.java
+++ UniqueNames.java
@@ -1,14 +1,30 @@
 import java.util.*;
+//complete
 public class UniqueNames {
 	static Scanner input = new Scanner(System.in);
 	static ArrayList<String> names = new ArrayList<String>();
 	public static void main(String[] args) {
 		nameInput();
+		//checks names arraylist for duplicates
+		for(int i = 0; i <= names.size(); i++) {
+			for(int x = i + 1; x < names.size(); x++) {
+				if(names.get(i).equalsIgnoreCase(names.get(x))) {
+					String removed = names.remove(i);
+					if(removed.equals(names.get(x)))names.remove(x);
+				}
+			}
+		}
+		System.out.println("Unique name list contains: ");
 		System.out.println(names);
 	}
+	//name input, breaks if input is blank
 	public static ArrayList<String> nameInput() {
+		String name = "initialized";
+		while(name.length() > 0) {
 		System.out.println("Enter name: ");
-		names.add(input.nextLine());
+		name = input.nextLine();
+		names.add(name);
+		}
 		return names;
 	}
 }
